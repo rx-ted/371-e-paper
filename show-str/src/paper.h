c4e8ce74dd0a2765e677cb2f92d2ac98aa5c3039
rx-ted <1552488649@qq.com>
2022-11-28 21:43:21 +0800

    add paltformio.ini and src folder

---
+++ show-str/src/paper.h
@@ -0,0 +1,31 @@
+/*
+ * @Author: rx-ted
+ * @Date: 2022-11-27 15:27:46
+ * @LastEditors: rx-ted
+ * @LastEditTime: 2022-11-27 15:28:56
+ */
+#ifndef __PAPAR_H
+#define __PAPAR_H
+
+
+
+class paper
+{
+private:
+    /* data */
+public:
+    paper(/* args */);
+    ~paper();
+};
+
+paper::paper(/* args */)
+{
+}
+
+paper::~paper()
+{
+}
+
+
+
+#endif
